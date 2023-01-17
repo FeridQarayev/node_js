da0aab6545c7de994d9e1f78836c92801bb87fea
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-17 23:43:22 +0400

    user update method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { v4: uuidv } = require("uuid");
 const PORT = 8080;
 
 const app = express();
+app.use(express.json());
 
 const users = [
   {
@@ -22,10 +23,12 @@ const users = [
     password: "Akif123",
   },
 ];
+// --------------------------Get All Users------------------------------------
 app.get("/api/users", (req, res) => {
   res.send({ message: "success", users });
 });
 
+// --------------------------Get User As Id------------------------------------
 app.get("/api/users/:id", (req, res) => {
   const { id } = req.params;
   let user = users.find((p) => p.id === id);
@@ -35,6 +38,22 @@ app.get("/api/users/:id", (req, res) => {
       user,
     });
   } else res.status(204).send();
+});
+
+// --------------------------User Update ------------------------------------
+app.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, surname, age } = req.body;
+  let user = users.find((p) => p.id === id);
+  if (!user) return res.status(204).send();
+  if (!name && !surname && !age)
+    return res
+      .status(400)
+      .send({ message: "Name or surname or age required!" });
+  if (name) user.name = name;
+  if (surname) user.surname = surname;
+  if (age) user.age = age;
+  res.send({ message: "User successfully updated!", user });
 });
 
 app.listen(PORT, () => {
