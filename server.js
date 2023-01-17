06d094a633118bf526010b7960c3447dc3ec6855
FeridQarayev <tu7he6n9s@code.edu.az>
2023-01-17 23:30:13 +0400

    get user methods

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 const express = require("express");
-const PORT = 8081;
+const { v4: uuidv } = require("uuid");
+const PORT = 8080;
 
-const App = express();
+const app = express();
 
-const products = [
+const users = [
   {
-    id: 1,  
-    name: "John",
-    isDeleyted: false,
+    id: "1",
+    name: "Alakisi",
+    surname: "Baxiseliyev",
+    age: 31,
+    email: "code@gamil.com",
+    password: "Akif123",
+  },
+  {
+    id: "2",
+    name: "Alakisi",
+    surname: "Baxiseliyev",
+    age: 31,
+    email: "code@gamil.com",
+    password: "Akif123",
   },
 ];
+
+app.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  let user = users.find((p) => p.id === id);
+  if (user) {
+    res.send({
+      message: "success",
+      user,
+    });
+  } else res.status(204).send();
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
